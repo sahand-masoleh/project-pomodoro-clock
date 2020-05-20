8e951c34b185ae2b02b3eb79a07688dc85436f8c
Sahand Masoleh <sahand.masoleh@gmail.com>
2020-05-21 00:52:53 +0800

    Updates

---
+++ precache-manifest.16ce740d24a2e7a08c5ff7fd8a6bc23e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54152f107c5f429dfdb861db639e1b3",
+    "url": "/project-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "1c00cbf9c6a9627f915d",
+    "url": "/project-pomodoro-clock/static/css/main.5043fe4c.chunk.css"
+  },
+  {
+    "revision": "70914808fc988434f450",
+    "url": "/project-pomodoro-clock/static/js/2.783cfa87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-pomodoro-clock/static/js/2.783cfa87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c00cbf9c6a9627f915d",
+    "url": "/project-pomodoro-clock/static/js/main.3047a0e6.chunk.js"
+  },
+  {
+    "revision": "8d0912c2ac1c55aa0929",
+    "url": "/project-pomodoro-clock/static/js/runtime-main.87db87e6.js"
+  },
+  {
+    "revision": "af0142b518dd86b50e95e2221e717394",
+    "url": "/project-pomodoro-clock/static/media/BalooChettan2-Regular.af0142b5.ttf"
+  },
+  {
+    "revision": "63f874d192fb3892d88d5e26f942b5e2",
+    "url": "/project-pomodoro-clock/static/media/DS-DIGI.63f874d1.TTF"
+  },
+  {
+    "revision": "d4fa95213c32659ed1b497cfe4139c7e",
+    "url": "/project-pomodoro-clock/static/media/beep.d4fa9521.mp3"
+  },
+  {
+    "revision": "fb26bb5718c0467742510c51db26282c",
+    "url": "/project-pomodoro-clock/static/media/tomato.fb26bb57.png"
+  }
+]);
